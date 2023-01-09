212dc4a86b362fe83c31905b78d31aa27eade901
andreaa21 <andrea.zollini@proton.me>
2023-01-09 16:33:25 +0100

    add db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,14 +1,20 @@
 <?php
 
-namespace App\Http\Controllers\Guest;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
+
+        $movie = Movie::all();
+
+        dd($movie);
+
         return view('home');
     }
 }
